56166df24a7966637b5856d23b38ccdcd2174835
phborba <philipeborba@gmail.com>
2021-03-04 13:13:18 -0300

    test script

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- pytorch_segmentation_models_trainer
+ segmentation_models_trainer
                               -------------------
-        begin                : 2021-03-02
+        begin                : 2021-03-04
         git sha              : $Format:%H$
-        copyright            : (C) 2021 by Philipe Borba - Cartographic Engineer
-                                                            @ Brazilian Army
+        copyright            : (C) 2021 by Philipe Borba - 
+                                    Cartographic Engineer @ Brazilian Army
         email                : philipeborba at gmail dot com
  ***************************************************************************/
+
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -19,24 +20,27 @@
  ****
 """
 
-import hydra
-from omegaconf import DictConfig
-from pytorch_lightning import Trainer
-from pytorch_segmentation_models_trainer.predict import predict
+import os
+import subprocess
+
+import pytorch_lightning as pl
+from hydra.experimental import compose, initialize
 from pytorch_segmentation_models_trainer.train import train
-from pytorch_segmentation_models_trainer.config_utils import validate_config
+
+from tests.utils import CustomTestCase
 
 
-@hydra.main()
-def main(cfg: DictConfig) -> Trainer:
-    if cfg.mode == 'train':
-        return train(cfg)
-    elif cfg.mode == 'predict':
-        return predict(cfg)
-    elif cfg.mode == 'validate-config':
-        return validate_config(cfg)
-    else:
-        raise NotImplementedError
-
-if __name__=="__main__":
-    main()
+class Test_TestScript(CustomTestCase):
+    def test_run_validate_config(self) -> None:
+        return_from_process = subprocess.run(
+            [
+                'pytorch-segmentation-models-trainer',
+                '--config-path',
+                '../tests/test_configs',
+                '--config-name',
+                'experiment',
+                '+mode=validate-config'
+            ],
+            check=True
+        )
+        self.assertEqual(return_from_process.returncode, 0)
