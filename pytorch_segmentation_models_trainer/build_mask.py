402a8ce4958019fbb1ab49ccaef6d913958d5033
phborba <philipeborba@gmail.com>
2021-05-06 15:36:42 -0300

    new test data and start of build mask with hydra

--- pytorch_segmentation_models_trainer/build_mask.py
+++ pytorch_segmentation_models_trainer/build_mask.py
@@ -28,48 +28,15 @@ from hydra.core.config_store import ConfigStore
 from hydra.utils import instantiate
 from omegaconf import MISSING, DictConfig, OmegaConf
 
+from pytorch_segmentation_models_trainer.tools.mask_building.mask_builder import (
+    BatchFileGeoDFConfig,
+    PostgisConfig,
+    FileGeoDFConfig,
+    MaskBuilder,
+    replicate_image_structure,
+)
+
 logger = logging.getLogger(__name__)
-
-@dataclass
-class VectorReaderConfig:
-    pass
-
-@dataclass
-class FileGeoDFConfig:
-    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.FileGeoDF"
-    file_name: str = "../tests/testing_data/data/vectors/test_polygons.geojson"
-
-@dataclass
-class PostgisConfig(VectorReaderConfig):
-    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.PostgisGeoDF"
-    database: str = "dataset_mestrado"
-    sql: str = "select id, geom from buildings"
-    user: str = "postgres"
-    password: str = "postgres"
-    host: str = "localhost"
-    port: int = 5432
-
-@dataclass
-class BatchFileGeoDFConfig(VectorReaderConfig):
-    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.BatchFileGeoDF"
-    root_dir: str = "../tests/testing_data/data/vectors"
-    file_extension: str = "geojson"
-
-@dataclass
-class MaskBuilder:
-    defaults: List[Any] = field(default_factory=lambda: [{"geo_df": "batch_file"}])
-    geo_df: VectorReaderConfig = MISSING
-    root_dir: str = '/data'
-    output_csv_path: str = '/data'
-    image_root_dir: str = 'images'
-    replicate_image_folder_structure: bool = True
-    relative_path_on_csv: bool = True
-    build_polygon_mask: bool = True
-    polygon_mask_folder_name: str = 'polygon_masks'
-    build_boundary_mask: bool = True
-    boundary_mask_folder_name: str = 'boundary_masks'
-    build_vertex_mask: bool = True
-    vertex_mask_folder_name: str = 'vertex_masks'
 
 cs = ConfigStore.instance()
 cs.store(name="config", node=MaskBuilder)
@@ -83,7 +50,7 @@ def build_masks(cfg: DictConfig):
         "Starting the training of a model with the following configuration: \n%s",
         OmegaConf.to_yaml(cfg)
     )
-    if cfg.geo_df.root_dir.startswith(".."):
+    if 'root_dir' in cfg.geo_df and cfg.geo_df.root_dir.startswith(".."):
         cfg.geo_df.root_dir = str(
             os.path.join(
                 os.path.dirname( __file__ ),
@@ -91,7 +58,8 @@ def build_masks(cfg: DictConfig):
             )
         )
     geo_df = instantiate(cfg.geo_df)
-    print(geo_df)
+    if cfg.replicate_image_folder_structure:
+        replicate_image_structure(cfg)
 
 if __name__=="__main__":
     build_masks()
--- pytorch_segmentation_models_trainer/tools/mask_building/mask_builder.py
+++ pytorch_segmentation_models_trainer/tools/mask_building/mask_builder.py
@@ -19,19 +19,76 @@
  ****
 """
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from omegaconf import MISSING, DictConfig, OmegaConf
 
 import rasterio
 from rasterio.plot import reshape_as_image
-from typing import List
+from typing import Any, List
 from pytorch_segmentation_models_trainer.tools.data_readers.raster_reader import (
     RasterFile, MaskOutputType
 )
 from pytorch_segmentation_models_trainer.tools.data_readers.vector_reader import (
     GeoDF, FileGeoDF, GeomTypeEnum, GeomType
 )
-from pytorch_segmentation_models_trainer.build_mask import MaskBuilder
+
+@dataclass
+class VectorReaderConfig:
+    pass
+
+@dataclass
+class FileGeoDFConfig:
+    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.FileGeoDF"
+    file_name: str = "../tests/testing_data/data/vectors/test_polygons.geojson"
+
+@dataclass
+class PostgisConfig(VectorReaderConfig):
+    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.PostgisGeoDF"
+    database: str = "dataset_mestrado"
+    sql: str = "select id, geom from buildings"
+    user: str = "postgres"
+    password: str = "postgres"
+    host: str = "localhost"
+    port: int = 5432
+
+@dataclass
+class BatchFileGeoDFConfig(VectorReaderConfig):
+    _target_: str = "pytorch_segmentation_models_trainer.tools.data_readers.vector_reader.BatchFileGeoDF"
+    root_dir: str = "../tests/testing_data/data/vectors"
+    file_extension: str = "geojson"
+
+@dataclass
+class MaskBuilder:
+    defaults: List[Any] = field(default_factory=lambda: [{"geo_df": "batch_file"}])
+    geo_df: VectorReaderConfig = MISSING
+    root_dir: str = '/data'
+    output_csv_path: str = '/data'
+    image_root_dir: str = 'images'
+    image_dir_is_relative_to_root_dir: str = True
+    replicate_image_folder_structure: bool = True
+    relative_path_on_csv: bool = True
+    build_polygon_mask: bool = True
+    polygon_mask_folder_name: str = 'polygon_masks'
+    build_boundary_mask: bool = True
+    boundary_mask_folder_name: str = 'boundary_masks'
+    build_vertex_mask: bool = True
+    vertex_mask_folder_name: str = 'vertex_masks'
+        
+def replicate_image_structure(cfg):
+    input_base_path = str(
+        os.path.join(cfg.root_dir, cfg.image_root_dir)
+    ) if cfg.image_dir_is_relative_to_root_dir else cfg.image_root_dir
+    for mask_type in ['polygon_mask', 'boundary_mask', 'vertex_mask']:
+        if getattr(cfg, f"build_{mask_type}"):
+            mask_folder_name = getattr(cfg, f"{mask_type}_folder_name")
+            output_base_path = str(
+                os.path.join(cfg.root_dir, mask_folder_name)
+            )
+            build_destination_dirs(
+                input_base_path=input_base_path,
+                output_base_path=output_base_path
+            )
 
 def build_mask_type_list(cfg: MaskBuilder):
     """[summary]
@@ -51,7 +108,17 @@ def build_mask_type_list(cfg: MaskBuilder):
         mask_type_list.append(GeomTypeEnum.POINT)
     return mask_type_list
 
-
+def build_destination_dirs(input_base_path: str, output_base_path: str):
+    if input_base_path == os.path.commonpath([input_base_path, output_base_path]) \
+        and not output_base_path.split(
+            os.path.commonpath([input_base_path, output_base_path])
+        )[-1].startswith("/.."):
+        raise Exception("input path must not be in output_path")
+    return [
+        Path(
+            dirpath.replace(input_base_path, output_base_path)    
+        ).mkdir(parents=True, exist_ok=True) for dirpath, _, __ in os.walk(input_base_path)
+    ]
 
 def build_mask_func(input_raster_path: str, input_vector: GeoDF, \
     output_dir: str, mask_type_list: List[GeomType], mask_output_type: MaskOutputType):
@@ -63,7 +130,6 @@ def build_mask_func(input_raster_path: str, input_vector: GeoDF, \
         mask_type_list (List[GeomType]): [description]
         mask_output_type (MaskOutputType): [description]
     """
-    Path(output_dir).mkdir(parents=True, exist_ok=True)
     raster_df = RasterFile(file_name=input_raster_path)
     return raster_df.build_mask(
         input_vector_layer=input_vector,
