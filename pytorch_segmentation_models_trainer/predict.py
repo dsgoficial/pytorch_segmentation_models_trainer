262b8def6ac2b7eac753778d9e3cd7894555bfcc
phborba <philipeborba@gmail.com>
2021-07-18 23:57:57 -0300

    first commit on main predictor

--- pytorch_segmentation_models_trainer/predict.py
+++ pytorch_segmentation_models_trainer/predict.py
@@ -19,17 +19,52 @@
  ****
 """
 import logging
+from pytorch_segmentation_models_trainer.tools.polygonization.polygonizer import TemplatePolygonizerProcessor
+from pytorch_segmentation_models_trainer.tools.inference.inference_processors import AbstractInferenceProcessor
 import hydra
 import omegaconf
 import rasterio
 from torch.utils.data import DataLoader
 import numpy as np
-from omegaconf import DictConfig
+from omegaconf import DictConfig, MISSING
+from dataclasses import dataclass, field
+
+@dataclass
+class PredictionConfig:
+    training_cfg: dict = field(default_factory=dict)
+    checkpoint_path: str = MISSING
+    polygonizer: TemplatePolygonizerProcessor = MISSING
+    inference_processor: AbstractInferenceProcessor = MISSING
+    image_reader: str = MISSING #TODO: define an object structure for this part
+    inference_threshold: float = 0.5
+
+def instantiate_model(model_cfg, checkpoint_path):
+    pass
+
+def instantiate_inference_processor(inference_processor_cfg, polygonizer=None):
+    pass
+
+def get_images(image_reader_cfg):
+    pass
 
 @hydra.main(config_path="conf", config_name="config")
 def predict(cfg: DictConfig):
-    print("Not implemented yet.")
-    pass
+    model = instantiate_model(
+        cfg.training_cfg.pl_model if "pl_model" in cfg.training_cfg else cfg.training_cfg.model,
+        cfg.checkpoint_path
+    )
+    inference_processor = instantiate_inference_processor(
+        cfg.inference_processor,
+        polygonizer=None if "polygonizer" not in cfg else cfg.polygonizer
+    )
+    images = get_images(cfg.image_reader)
+    for image in images:
+        inference_processor.process(
+            image,
+            threshold=cfg.inference_threshold
+        )
+
+
 
 
 if __name__=="__main__":
