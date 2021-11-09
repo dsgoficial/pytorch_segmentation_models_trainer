d4fdaa7425d0ed7864ab76f0f132358e259218ed
phborba <philipeborba@gmail.com>
2021-11-08 22:27:54 -0300

    polygonrnn bug fix

--- pytorch_segmentation_models_trainer/utils/polygonrnn_utils.py
+++ pytorch_segmentation_models_trainer/utils/polygonrnn_utils.py
@@ -28,6 +28,7 @@ import torch
 import itertools
 from shapely.geometry import Polygon, LineString, Point
 from shapely.geometry.base import BaseGeometry
+import cv2
 
 
 def label2vertex(labels):
@@ -77,6 +78,50 @@ def get_vertex_list(
                 lambda x: x != grid_size * grid_size, input_list
             )
         ]
+    )
+
+
+def get_vertex_list_from_numpy(
+    input_array: np.array,
+    scale_h: Optional[float] = 1.0,
+    scale_w: Optional[float] = 1.0,
+    min_col: Optional[int] = 0,
+    min_row: Optional[int] = 0,
+    grid_size: Optional[int] = 28,
+    return_cast_func: Optional[Callable] = None,
+) -> np.array:
+    """Gets vertex list from input batch.
+
+    Args:
+        input_batch (np.array): [description]
+        scale_h (Optional[float], optional): Height scale. Defaults to 1.0.
+        scale_w (Optional[float], optional): Width scale. Defaults to 1.0.
+        min_col (Optional[int], optional): Minimum column. Defaults to 0.
+        min_row (Optional[int], optional): Minimun row. Defaults to 0.
+    """
+    return_cast_func = return_cast_func if return_cast_func is not None else lambda x: x
+    cast_to_int = lambda x: x.astype(np.int32) if isinstance(x, np.ndarray) else x.int()
+    cast_to_np = lambda x: x.cpu().numpy() if isinstance(x, torch.Tensor) else x
+    input_array = cast_to_np(input_array)
+    if np.max(input_array) >= grid_size ** 2:
+        length = np.argmax(input_array)
+        input_array = input_array[:length]
+    if input_array.shape[0] == 0:
+        return input_array
+    poly = np.stack(
+        [cast_to_int(input_array % grid_size), cast_to_int(input_array // grid_size)],
+        axis=-1,
+    )
+    poly01 = poly0g_to_poly01(poly, grid_size)
+    polyg = poly01_to_poly0g(poly01, 224).astype(np.float32)
+    return return_cast_func(
+        np.stack(
+            [
+                polyg[:-2][:, 0] / cast_to_np(scale_w) + cast_to_np(min_col),
+                polyg[:-2][:, 1] / cast_to_np(scale_h) + cast_to_np(min_row),
+            ],
+            axis=-1,
+        )
     )
 
 
@@ -152,11 +197,9 @@ def get_vertex_list_from_batch_tensors(
         min_col (Optional[Union(int, torch.Tensor)], optional): Minimum column. Defaults to 0.
         min_row (Optional[Union(int, torch.Tensor)], optional): Minimun row. Defaults to 0.
     """
-    cast_func = lambda x: torch.tensor(
-        x, dtype=torch.float32, device=input_batch.device
-    )
+    cast_func = lambda x: np.array(x, dtype=np.float32)
     return [
-        get_vertex_list(
+        get_vertex_list_from_numpy(
             x,
             scale_h[idx],
             scale_w[idx],
@@ -165,8 +208,6 @@ def get_vertex_list_from_batch_tensors(
             return_cast_func=cast_func,
             grid_size=grid_size,
         )
-        .cpu()
-        .numpy()
         for idx, x in enumerate(torch.unbind(input_batch, dim=0))
     ]
 
@@ -211,10 +252,35 @@ def tensor2img(tensor):
     return img
 
 
+def poly01_to_poly0g(poly, grid_size):
+    """
+    [0, 1] coordinates to [0, grid_size] coordinates
+
+    Note: simplification is done at a reduced scale
+    """
+    poly = np.floor(poly * grid_size).astype(np.int32)
+    # poly = cv2.approxPolyDP(poly, 0, False)[:, 0, :]
+
+    return poly
+
+
+def poly0g_to_poly01(polygon, grid_side):
+    """
+    [0, grid_side] coordinates to [0, 1].
+    Note: we add 0.5 to the vertices so that the points
+    lie in the middle of the cell.
+    """
+    result = (polygon.astype(np.float32) + 0.5) / grid_side
+
+    return result
+
+
 def build_arrays(polygon, num_vertexes, sequence_length, grid_size=28):
     point_count = 2
     label_array = np.zeros([sequence_length, grid_size * grid_size + 3])
     label_index_array = np.zeros([sequence_length])
+    polygon = poly0g_to_poly01(polygon, 224)
+    polygon = poly01_to_poly0g(polygon, grid_size)
     if num_vertexes < sequence_length - 3:
         for points in polygon:
             _initialize_label_index_array(
@@ -263,8 +329,8 @@ def _populate_label_index_array(
         elif kkk % (num_vertexes + 3) == 1:
             index = grid_size * grid_size + 2
         else:
-            index_a = int(polygon[kkk % (num_vertexes + 3) - 2][0] / 8)
-            index_b = int(polygon[kkk % (num_vertexes + 3) - 2][1] / 8)
+            index_a = polygon[kkk % (num_vertexes + 3) - 2][0]
+            index_b = polygon[kkk % (num_vertexes + 3) - 2][1]
             index = index_b * grid_size + index_a
         label_array[kkk, index] = 1
         label_index_array[kkk] = index
@@ -273,9 +339,10 @@ def _populate_label_index_array(
 def _initialize_label_index_array(
     point_count, label_array, label_index_array, points, grid_size=28
 ):
-    index_a = int(points[0] / 8)
-    index_b = int(points[1] / 8)
-    index = index_b * grid_size + index_a
+    # index_a = int(points[0] / 8)
+    # index_b = int(points[1] / 8)
+    # index = index_b * grid_size + index_a
+    index = points[0] + points[1] * grid_size
     label_array[point_count, index] = 1
     label_index_array[point_count] = index
 
@@ -290,4 +357,4 @@ def handle_vertices(vertices):
         return Point(vertices_array.squeeze(0))
     if vertices_array.shape[0] == 2:
         return LineString(vertices_array)
-    return Polygon(vertices_array.reshape(-1, 2)).convex_hull
+    return Polygon(vertices_array.reshape(-1, 2)).boundary
